15dac3a158881cdeea398a85f0e9f55ff9855f51
Armaan Singh <socials.cwa@gmail.com>
2026-08-02 14:18:16 +0530

    Build responsive mobile navigation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "192.168.0.144",
+    "100.120.225.73",
+  ],
 };
 
 export default nextConfig;
